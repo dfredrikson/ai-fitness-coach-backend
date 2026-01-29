5a87d70164c47dade55bcbff60bc2972d3040513
Daniel Fredrikson <Dani.fredrikson@gmail.com>
2026-01-29 13:36:52 -0300

    fix import models daily and activity motivation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,5 @@ from app.models.activity import Activity, AIAnalysis
 from app.models.coach_personality import CoachPersonality
 from app.models.routine import Routine, RoutineDay
 from app.models.message import Message
+from app.models.daily_motivation import DailyMotivation
+from app.models.activity_motivation import ActivityMotivation
